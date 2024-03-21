bba44d32b3b824069cc2ed663978ee78a7cb3118
Joel Roshan <joeljosephroshan@gmail.com>
2024-03-21 08:42:00 +0530

    images changed

--- cuwa/src/app/products/page.jsx
+++ cuwa/src/app/products/page.jsx
@@ -1,39 +1,52 @@
 import React from 'react'
 import "./page.css"
 import Productcard from "../../components/productcard/productcard"
-import Ocean from "../../assets/ocean1.svg"
+import Oceanimg from "../../assets/ocean1.svg"
+import Delightimg from "../../assets/DELIGHT.jpg"
+import Hulkimg from "../../assets/HULK.jpg"
+import Forstyimg from "../../assets/FORSTY.jpg"
+import Chillsipimg from "../../assets/CHILL SIP.jpg"
+import Forstydwarfimg from "../../assets/FORSTY DWARF.jpg"
+import Delighthandyimg from "../../assets/HANDY DELIGHT.jpg"
 const page = () => {
   return (
     <div className='main'>
         <h2 className='title'>Our Products</h2>
         <div className="container">
         <Productcard
+          img={Oceanimg}
           capacity="1 L"
           name="OCEAN"
         />
         <Productcard
+          img={Delightimg}
           capacity="1 L"
           name="DELIGHT"
         />  
         <Productcard
+        img={Hulkimg}
         capacity="1 L"
         name="HULK"
         />
         <Productcard
+          img={Forstyimg}
           capacity="500 mL"
           name="FORSTY"
         />
         <Productcard
+        img={Delighthandyimg}
         capacity="500 mL"
         name="DELIGHT HANDY"
         />
 
         <Productcard
+        img={Forstydwarfimg}
         capacity="300 mL"
         name="FORSTY DWARF"
         />
 
         <Productcard
+          img={Chillsipimg}
           capacity="300 mL"
           name="CHILL SIP"
         />
--- cuwa/src/components/productcard/productcard.jsx
+++ cuwa/src/components/productcard/productcard.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import "./productcard.css"
 import Image from 'next/image'
-import Productimg from "../../assets/ocean1.svg"
 const productcard = (props) => {
   return (
     <div className='productcard'>
-        <div className="productimg"><Image className="productimg" src={Productimg} priority/></div>
+        <div className="productimg"><Image className="productimg" src={props.img} priorityxq/></div>
         <p className="name">{props.name}</p>
         <p className="capacity">{props.capacity}</p>
 
